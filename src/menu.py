22e4b78b65e8f519804e60cce7e719577db8d742
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-11 19:18:29 -0400

    feat(utils): implement numeric input reading function with validation for non-integer values

--- src/menu.py
+++ src/menu.py
@@ -4,3 +4,16 @@ def clear_terminal():
 
     input('Press ENTER to continue...')
     os.system('cls' if os.name == 'nt' else 'clear')
+
+    return
+
+def read_numeric_option():
+
+    while True:
+
+        try:
+            return int(input('Choose one of the options above -> '))
+        except ValueError:
+            print('[ERRO] Invalid value!\n')
+            continue
+
