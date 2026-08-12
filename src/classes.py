4dd092ac4b1a0b5a881317da2f09827c7a9ffb05
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-12 11:34:22 -0400

    feat(binarytree): initialize binary tree class instances and conditions

--- src/classes.py
+++ src/classes.py
@@ -1,10 +1,24 @@
 class Node:
+    
     def __init__(self, data):
         self.data = data
         self.left = None # <- Remember that the binary tree model requires the developer to implement
         self.right = None # two pieces of data in cascade sequence
-
         return
 
     def __str__(self): # Returns the given data as a string to avoid type compatibility issues
         return str(self.data)
+
+
+class BinaryTree:
+
+    def __init__(self, data=None, node=None):
+        if node:
+            self.root = node # <- Build a subtree starting from a given node
+        elif data:
+            node = Node(data) # <- Initialize root from raw data
+            self.root = node
+        else:
+            self.root = None # <- Default to empty root
+        return
+        
