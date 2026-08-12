9f843c5ccc70ada310b75f0c05ffcb21d3e40269
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-12 10:28:56 -0400

    feat(node): implement Node class responsible for node generation and data storage

---
+++ src/classes.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None # <- Remember that the binary tree model requires the developer to implement
+        self.right = None # two pieces of data in cascade sequence
+
+        return
