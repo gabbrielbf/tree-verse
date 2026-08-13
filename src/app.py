9737df745389072cc3ad5461f21db15681111de5
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-13 14:55:39 -0400

    feat(app): create main app file to structure the usage of classes, methods, and functions

---
+++ src/app.py
@@ -0,0 +1,23 @@
+from tree import BinarySearchTree
+from menu import numbered_menu, clear_terminal
+
+while True:
+
+    match numbered_menu:
+
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            pass
+        case 6:
+            pass
+        case 7:
+            pass
+
+    clear_terminal()
