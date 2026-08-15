950eed39593458e3d7db100df6d2a6f78a51e2ff
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-15 17:53:37 -0400

    chore(lessons): adjust punctuation and fix text duplication in user display screens

--- src/lessons.py
+++ src/lessons.py
@@ -25,14 +25,13 @@ def banner():
         time.sleep(0.04)
     print()
 
-    return BANNER
+    return 
 
 def what_is_nodes():
 
     _temp_stack = """ - What are NODES? Connection of non-linear information 
-    through a specific root """
+through a specific root. """
     NODES = "\n".join(l.center(57) for l in _temp_stack.splitlines())
-    print(NODES)
 
     print(DIVIDER)
         
@@ -47,7 +46,7 @@ def what_is_nodes():
 def what_is_leaves():
 
     _temp_stack = """ - What are LEAVES? Isolated nodes at the end of 
-    trees with no connections below them """
+trees with no connections below them. """
     LEAVES = "\n".join(l.center(57) for l in _temp_stack.splitlines())
 
     print(DIVIDER)
@@ -62,8 +61,9 @@ def what_is_leaves():
 
 def what_is_binarytree():
 
-    _temp_stack = """ - What is a BINARYTREE? It is a tree model that forces the developer 
-    to provide exactly 2 children for every new node that appears in the data structure"""
+    _temp_stack = """ - What is a BINARYTREE? It is a tree model that forces 
+the developer to provide exactly 2 children for every 
+new node that appears in the data structure. """
     BINARYTREE = "\n".join(l.center(57) for l in _temp_stack.splitlines())
 
     print(DIVIDER)
@@ -90,7 +90,7 @@ def what_is_height_depht_paths():
     print()
 
     _temp_stack = """ 1. Height - It is the length of the path between the root 
-    and the leaf of greatest depth """
+and the leaf of greatest depth. """
     HEIGHT = "\n".join(l.center(57) for l in _temp_stack.splitlines())
 
     for char in HEIGHT: 
@@ -100,7 +100,7 @@ def what_is_height_depht_paths():
     print()
 
     _temp_stack = """ 2. Depht - It is the path distance from a given node to be 
-    selected by the user to the sentinel root of the tree"""
+selected by the user to the sentinel root of the tree."""
     DEPTH = "\n".join(l.center(57) for l in _temp_stack.splitlines())
 
     for char in DEPTH: 
@@ -110,7 +110,7 @@ def what_is_height_depht_paths():
     print()
 
     _temp_stack = """ 3. Path - It is the sequence of edges (connections) you 
-    need to traverse to go from one node to another"""
+    need to traverse to go from one node to another."""
     PATH = "\n".join(l.center(57) for l in _temp_stack.splitlines())
 
     for char in PATH: 
@@ -121,3 +121,8 @@ def what_is_height_depht_paths():
 
     return
 
+banner()
+what_is_nodes()
+what_is_leaves()
+what_is_binarytree()
+what_is_height_depht_paths()
