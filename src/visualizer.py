c06ebbb5b81e8fb67d7ff0745bd8f73793e4b92b
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-14 20:02:15 -0400

    feat(visualizer): implement node-by-node terminal display logic for the user (using _write)

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,2 +1,36 @@
 from menu import traversals
-from tree import BinarySearchTree
+import time, sys
+
+class Visualizer:
+
+    def __init__(self, bst):
+        self.bst = bst
+
+    def _write(self, text, delay=0.04):
+        
+        for char in str(text):
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(delay)
+
+
+def wich_traversal(bst):
+
+    visualizer = Visualizer(bst)
+
+    match traversals():
+        case 1:
+            pass
+
+        case 2:
+            pass
+
+        case 3:
+            pass
+
+        case 4:
+            pass
+
+        case 5:
+            pass
+
