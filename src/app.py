7794f0877644fda80605d36f0306c865df69aea0
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-17 16:28:52 -0400

    feat(app): add teach_user function to the main application for dynamic program presentation

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 from tree import BinarySearchTree
-from menu import numbered_menu, clear_terminal
+from menu import numbered_menu, clear_terminal, teach_user
 from visualizer import wich_traversal
 
 bst = BinarySearchTree()
 
 def run_code():
 
+    teach_user()
     while True:
-
+        
         clear_terminal()
         match numbered_menu():
 
