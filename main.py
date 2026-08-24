8c3e620a74f6044eaa130747e4056037bbb3afb3
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-24 11:18:42 -0400

    feat(main): create main entry point file for the user

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.app import run_code
+
+def main():
+    """ Function that will be used by 
+    the user, clean and straight to the point """
+
+    run_code()
+
+if __name__ == '__main__':
+    main()
