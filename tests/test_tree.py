dbfa6bad0f1f4306118babd62f5b88a15140ebdd
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-26 17:52:58 -0400

    refactor(test): define single function to check left and right node values

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -2,9 +2,10 @@ from src.tree import BinarySearchTree
 
 bst = BinarySearchTree()
 
-def test_nodeleft_behavior():
-    """ This test will check if the tree has
-    smaller values on the left than on the right """
+def test_nodes_behavior():
+    """ This test will check if the tree has values on the left 
+    that are smaller than the values on the right and the ones 
+    on the right are greater than the ones on the left """
 
     bst.insert(50)
     bst.insert(25)
@@ -13,5 +14,16 @@ def test_nodeleft_behavior():
     assert bst.root.data == 50 # Checking if the root value is 50
     assert bst.root.left.data == 25
     assert bst.root.right.data == 75
-    assert bst.root.left.data < bst.root.data # Checking if the left node value is smaller than the root
-    assert bst.root.data < bst.root.right.data # Checking if the root value is smaller than the right node
+
+    # Checking left
+    assert bst.root.left.data < bst.root.data # If the left node value is smaller than the root
+    assert bst.root.data < bst.root.right.data # If the root value is smaller than the right node
+
+    # Checking right
+    assert bst.root.right.data > bst.root.data 
+    assert bst.root.data > bst.root.left.data 
+
+
+
+
+    
